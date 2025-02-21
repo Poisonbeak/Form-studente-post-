f0558e63a5ae32a3e0d4a43cbf5ae6f91e739cba
Poisonbeak <michele.alfano@davincimilazzo.edu.it>
2025-02-21 08:35:41 +0100

    comments and fixes

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3300;
 
-app.use(express.static(__dirname))
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname))              // fornisce i file delle pagine direttamente dalla cartella
+app.use(express.json());                        // converte le fetch da raw data in oggetto
+app.use(express.urlencoded({extended:true}));   // conversione per le post
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/" + "index.html");
+    res.sendFile(__dirname + "/index.html");
 })
 
 app.listen(port, () => {
