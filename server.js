21946c10c865626b621596aba7768f7c917bb8b5
Poisonbeak <michele.alfano@davincimilazzo.edu.it>
2025-02-21 08:31:40 +0100

    get index and listen methods

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("Hello")
+const express = require("express");
+const app = express();
+const port = 3300;
+
+app.use(express.static(__dirname))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/" + "index.html");
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
